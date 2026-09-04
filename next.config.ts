2986e4aa808ab2b680aa8c9a7fe39b746d3594d1
Juan.vasquez <juan.vasquez@safetyclub.cl>
2026-09-04 19:22:29 -0400

    fix: workflows

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
 
-  basePath: "/antu-kuyen-landing",
+  basePath: "/lading-antu-kuyen",
   trailingSlash: true,
   images: {
     unoptimized: true,
